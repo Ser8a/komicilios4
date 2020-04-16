27f64e23e1109a4bd6000ca746aafb2fdc645946
Sergio Ochoa <ser8a76@hotmail.com>
2020-04-16 16:08:51 -0500

    Modificada la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-//Verificar si puede cargar el service worker
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
-}
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
 
+if (navigator.serviceWorker) {
+
+    if (url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+
+
+    
+    navigator.serviceWorker.register(swLocation);
+
+}
 
 // Referencias de jQuery
 
